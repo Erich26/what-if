8991d2857f211113ea2a37c992e2940df7c97616
Erich26 <erich.fredersdorff@codeimmersives.com>
2021-09-21 15:38:44 -0400

    fix group-sizes

--- group-sizes.js
+++ group-sizes.js
@@ -1,29 +1,19 @@
 const prompt = require('prompt-sync')();
 
 const classsize = Number(prompt('Please enter your class size: '));
-const groupsize = 3;
-const remaininggroups = 2;
 
-const groups = classsize / groupsize;
-
-
-const i = classsize % groupsize;
-const i2 = classsize - i; 
-const solution = i2 / groupsize;
-
-const almost = solution / remaininggroups;
-
-const nearly = (solution * 3) - (almost * remaininggroups);
-const groupsthree = nearly / groupsize;
-const groupstwo = (classsize - nearly) / remaininggroups;
-
-if (groups % 3 === 0 ) {
-    console.log('Groups of three: ', groups );
-} 
-else {
-    console.log('Groups of three: ', groupsthree, 'Groups of two: ', groupstwo )
+if (classsize % 3 === 0) {
+   console.log(classsize / 3, 'Groups of three')
 }
 
+else if (classsize % 3 === 1) {
+    numGroupsOfThree = (classsize - 4) / 3;
+    numGroupsOfTwo = 2;
+    console.log(numGroupsOfThree, 'Groups of three', numGroupsOfTwo, 'Groups of two');
+}
 
-
-
+else if (classsize % 3 === 2) {
+    numGroupsOfThree = (classsize - 2) / 3;
+    numGroupsOfTwo = 1;
+    console.log(numGroupsOfThree, 'Groups of three', numGroupsOfTwo, 'Groups of two');
+}
